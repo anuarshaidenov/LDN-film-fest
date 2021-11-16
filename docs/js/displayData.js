cf6da94d4f4739625c4029ac804a5ddb74110c1b
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-11-16 19:31:32 +0600

    Display creatives data dynamically

---
+++ docs/js/displayData.js
@@ -0,0 +1,20 @@
+export function displayData(container, data) {
+  data.forEach((entry) => {
+    const markup = `
+  <li class="creative-card">
+  <img
+    src="${entry.img}"
+    alt="${entry.name} picture"
+    class="creative-card__img"
+  />
+  <h3 class="creative-card__name">${entry.name}</h3>
+  <p class="creative-card__role">${entry.role}</p>
+  <p class="creative-card__description">
+    ${entry.description}
+  </p>
+</li>
+  `;
+
+    container.insertAdjacentHTML('beforeend', markup);
+  });
+}
--- docs/js/index.js
+++ docs/js/index.js
@@ -1,9 +1,58 @@
 import { stickHeader, stickHamburger } from './sticky.js';
 import * as mobileMenu from './mobileMenu.js';
 import { displayCurrentLink } from './currentPage.js';
+import { displayData } from './displayData.js';
 
 const links = document.querySelectorAll('.nav-link a');
 const mobileLinks = document.querySelectorAll('.mobile-menu__nav-link a');
+const creativesContainer = document.getElementById('creatives');
+
+const creatives = [
+  {
+    name: 'Mahershalla Ali',
+    role: 'An actor and a rapper',
+    description:
+      'Recipient of multiple accolades, including two Academy Awards, three Screen Actors Guild Awards, a BAFTA award, a Golden Globe Award and a Primetime Emmy Award.',
+    img: './images/mahershalla.png',
+  },
+  {
+    name: 'Steven Yeun',
+    role: 'An actor',
+    description:
+      'Recipient of multiple accolades, including two Academy Awards, three Screen Actors Guild Awards, a BAFTA award, a Golden Globe Award and a Primetime Emmy Award.',
+    img: './images/steven.jpeg',
+  },
+  {
+    name: 'Zoe Kravitz',
+    role: 'An actress',
+    description:
+      'Recipient of multiple accolades, including two Academy Awards, three Screen Actors Guild Awards, a BAFTA award, a Golden Globe Award and a Primetime Emmy Award.',
+    img: './images/zoe-kravitz.jpeg',
+  },
+  {
+    name: 'Denzel Washington',
+    role: 'An actor',
+    description:
+      'Recipient of multiple accolades, including two Academy Awards, three Screen Actors Guild Awards, a BAFTA award, a Golden Globe Award and a Primetime Emmy Award.',
+    img: './images/denzel.jpeg',
+  },
+  {
+    name: 'Brad Pitt',
+    role: 'An actor',
+    description:
+      'Recipient of multiple accolades, including two Academy Awards, three Screen Actors Guild Awards, a BAFTA award, a Golden Globe Award and a Primetime Emmy Award.',
+    img: './images/brad-pitt.jpeg',
+  },
+  {
+    name: 'Quentin Tarantino',
+    role: 'A film director',
+    description:
+      'Recipient of multiple accolades, including two Academy Awards, three Screen Actors Guild Awards, a BAFTA award, a Golden Globe Award and a Primetime Emmy Award. Director of Pupl Fiction.',
+    img: './images/quentin.jpeg',
+  },
+];
+
+displayData(creativesContainer, creatives);
 
 window.addEventListener('scroll', stickHeader);
 window.addEventListener('scroll', stickHamburger);
