9add6c2ac430fe988fdc1ed1cb9ebd6ab0ae4a9a
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-11-16 21:05:28 +0600

    Fix sticky hamburger menu bug

--- docs/js/mobileMenu.js
+++ docs/js/mobileMenu.js
@@ -1,7 +1,6 @@
 const body = document.querySelector('body');
 const html = document.querySelector('html');
 const menu = document.getElementById('menu');
-const hamburgerContainer = document.getElementById('hamburger-container');
 export const btnHamburger = document.getElementById('hamburger');
 export const btnClose = document.getElementById('close');
 
@@ -9,7 +8,6 @@ export function openMenu() {
   body.style.overflow = 'hidden';
   html.style.overflow = 'hidden';
 
-  hamburgerContainer.classList.remove('hamburger-sticky');
   btnHamburger.classList.add('hidden');
   btnClose.classList.remove('hidden');
   menu.classList.remove('translated');
@@ -19,7 +17,6 @@ export function closeMenu() {
   body.style.overflow = 'visible';
   html.style.overflow = 'visible';
 
-  hamburgerContainer.classList.add('hamburger-sticky');
   btnClose.classList.add('hidden');
   btnHamburger.classList.remove('hidden');
   menu.classList.add('translated');
